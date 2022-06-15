c9e54be933a856f724f1a57d683440075481b4fe
delicha <shoheiyamamoto919@gmail.com>
2022-06-16 02:23:04 +0900

    add admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+
   root to: 'home#index'
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    passwords:     'users/passwords',
     sessions:      'users/sessions',
-   }
+  }
 
   resources :users do
     post :details
